b929e78a13088fc9bc4b352580ce14c6a905363f
anjalidalal <anjalimaria2002@gmail.com>
2023-04-11 15:19:10 +0530

    Routing and state management in redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="fixed inset-0  bg-gray-800 flex items-center w-screen h-10 justify-between px-2 py-6">
+    <div className="fixed inset-0  bg-white shadow-md flex items-center w-screen h-10 justify-between px-2 py-6">
       <div className="flex items-center gap-1 mx-1">
-        <h1 className="text-white text-2xl font-bold">Crypto Currency</h1>
+        <h1 className="text-slate-800 text-2xl font-bold">Crypto Currency</h1>
         <img src="./crypto.png" className="w-9 h-9" alt="" />
       </div>
       <Link to="/profile">
-        <img src="./profile.png" className="w-8 h-8 mx-1" alt="" />
+        <div className="bg-gray-950 w-9 h-9 border-2 border-black rounded-full">
+          <img src="./profile.png" className="w-full h-full" alt="" />
+        </div>
       </Link>
     </div>
   );
